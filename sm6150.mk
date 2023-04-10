aafa86a50a1fb943a8ccb25bfb588806f3fe3c11
JYRRC <javixhtsweet@gmail.com>
2023-04-10 18:52:05 +0200

    update_coltOS

--- sm6150.mk
+++ sm6150.mk
@@ -294,7 +294,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pixys
+    $(LOCAL_PATH)/overlay-colt
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
